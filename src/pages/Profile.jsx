31d21ead7cd4fcfb7e3b2262b8675b7773419473
Koushik Saha <93317898+Gituatu@users.noreply.github.com>
2023-07-12 20:16:30 +0530

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,16 +3,16 @@ import { Context } from "../main";
 import Loader from "../components/Loader";
 
 const Profile = () => {
-  const { isAuthenticated, user } = useContext(Context);
+  const { isAuthenticated, loading, user } = useContext(Context);
 
-  if(!isAuthenticated) return (<Loader/>)
-  if(isAuthenticated) 
-    return (
+  return loading ? (
+    <Loader />
+  ) : (
     <div>
       <h1>{user?.name}</h1>
       <p>{user?.email}</p>
     </div>
-    )
+  );
 };
 
 export default Profile;
